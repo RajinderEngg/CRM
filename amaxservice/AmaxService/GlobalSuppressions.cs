4b96265f46e0c0aa640c26652da476a6cf2af282
Rajinder kumar <rajinder.eng@gmail.com>
2021-04-08 18:12:57 +0530

    Delete amaxservice directory

--- amaxservice/AmaxService/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("General", "RCS1023:Format block.", Justification = "<Pending>", Scope = "member", Target = "~M:AmaxService.CrmService.GetTreeData(System.IO.Stream)~System.IO.Stream")]
-
